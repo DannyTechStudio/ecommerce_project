0ecc698ad8c211a09b74a065a5d19f77a2563ae4
Daniel Offei <archimedes301@gmail.com>
2026-05-05 20:21:02 +0000

    feat: register promotion URLs in the project level URL conf

--- retailprime/urls.py
+++ retailprime/urls.py
@@ -33,4 +33,5 @@ urlpatterns = [
     path('webhook/', PayStackWebhookView.as_view()),
     path('api/', include('review.urls')),
     path('api/wishlist/', include('wishlist.urls')),
+    path('api/promotion/', include('promotion.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
