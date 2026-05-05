fc0df1e78e6e0df7b7c48f19b5ba4b3b4fe023d2
Daniel Offei <archimedes301@gmail.com>
2026-05-05 19:45:06 +0000

    feat(cart): add API views for coupon application and removal on cart

--- cart/views.py
+++ cart/views.py
@@ -2,14 +2,15 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from django.shortcuts import get_object_or_404
+from django.contrib.auth import get_user_model
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.generics import ListAPIView
-from django.contrib.auth import get_user_model
 
 from accounts.models import Address
-from .models import Cart, CartItem
+from promotion.services import CouponService
+from .models import CartStatus, Cart, CartItem
 from .services import CartService
-from .serializers import CartSerializer, AddToCartSerializer, UpdateCartItemSerializer, CheckoutResponseSerializer
+from .serializers import CartSerializer, ApplyCouponSerializer, AddToCartSerializer, UpdateCartItemSerializer, CheckoutResponseSerializer
 
 User = get_user_model()
 
@@ -22,6 +23,58 @@ class CartView(APIView):
     def get(self, request):
         cart = CartService.get_or_create_active_cart(request.user)
         return Response(CartSerializer(cart).data)
+
+
+class ApplyCouponView(APIView):
+    permission_classes = [IsAuthenticated]
+    
+    def post(self, request):
+        serializer = ApplyCouponSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        
+        code = serializer.validated_data["code"]
+        
+        cart = CartService.get_or_create_active_cart(request.user)
+        
+        try:
+            coupon = CouponService.validate_coupon(
+                user=request.user,
+                cart=cart,
+                coupon_code=code
+            )
+            
+            CartService.apply_coupon_to_cart(cart, coupon)
+            
+        except ValueError as e:
+            return Response(
+                {"detail": str(e)},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        
+        cart.refresh_from_db()
+        
+        return Response(
+            CartSerializer(cart).data,
+            status=status.HTTP_200_OK
+        )
+    
+    
+class RemoveCouponView(APIView):
+    permission_classes = [IsAuthenticated]
+    
+    def delete(self, request):
+        cart = CartService.get_or_create_active_cart(request.user)
+        
+        try:
+            CartService.remove_coupon_from_cart(cart)
+        except ValueError as e:
+            return Response(
+                {"detail": str(e)},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        
+        return Response(CartSerializer(cart).data, status=status.HTTP_200_OK)
+        # return Response(status=status.HTTP_204_NO_CONTENT)
     
     
 class AddToCartView(APIView):
@@ -79,10 +132,17 @@ class RemoveCartItemView(APIView):
             CartItem,
             id=item_id,
             cart__user=request.user,
-            cart__status="ACTIVE",
+            cart__status=CartStatus.ACTIVE
         )
         
-        cart = CartService.remove_cart_item(item)
+        try:
+            cart = CartService.remove_cart_item(item)
+            
+        except ValueError as e:
+            return Response(
+                {"detail": str(e)},
+                status=status.HTTP_400_BAD_REQUEST
+            )
         
         return Response(
             CartSerializer(cart).data,
@@ -118,6 +178,11 @@ class CheckoutCartView(APIView):
                 {"detail": str(e)},
                 status=status.HTTP_400_BAD_REQUEST
             )
+        except Exception:
+            return Response(
+                {"detail": "Something went wrong"},
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
             
         serializer = CheckoutResponseSerializer(result)
         
@@ -147,5 +212,13 @@ class AdminUserCartHistoryView(ListAPIView):
     serializer_class = CartSerializer
     
     def get_queryset(self):
-        user_id = self.kwargs["user_id"] 
-        return Cart.objects.filter(user_id=user_id).order_by("-created_at")
+        user_id = self.kwargs.get("user_id")
+        user = get_object_or_404(User, id=user_id)
+        
+        return (
+            Cart.objects
+            .filter(user=user)
+            .select_related("user")
+            .prefetch_related("items__product")
+            .order_by("-created_at")
+        )
