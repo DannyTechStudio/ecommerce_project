617f3fa0f6664405abeb38514126e78eca274a49
Daniel Offei <archimedes301@gmail.com>
2026-05-17 08:13:40 +0000

    feat(payment): add logs to track payment activity in the Paystack webhook view

--- payment/webhooks.py
+++ payment/webhooks.py
@@ -9,8 +9,9 @@ from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
 from django.conf import settings
 
-from .models import Payment, PaymentEvent
+from .models import Payment, PaymentEvent, PaymentStatus
 from .services import PaymentService
+from order.models import OrderStatus
 
 logger = logging.getLogger(__name__)
 
@@ -18,11 +19,14 @@ logger = logging.getLogger(__name__)
 @method_decorator(csrf_exempt, name="dispatch")
 class PayStackWebhookView(View):
     def post(self, request):
-        # logger.info(f"Webhook event: {event} | Ref: {reference}")
-        print("webook hit view")
+        logger.info("Webhook endpoint hit")
         
         payload = request.body
-        signature = request.headers.get("x-paystack-signature")
+        
+        signature = (
+            request.headers.get("x-paystack-signature")
+            or request.META.get("HTTP_X_PAYSTACK_SIGNATURE")
+        )
         
         computed_hash = hmac.new(
             settings.PAYSTACK_SECRET_KEY.encode(),
@@ -38,14 +42,22 @@ class PayStackWebhookView(View):
         event = data.get("event")
         reference = data.get("data", {}).get("reference")
         
-        logger.info(f"Webhook event received: {event}")
+        logger.info(f"Webhook event received: {event}, Ref: {reference}")
         
-        payment = None
-        if reference:
-            payment = Payment.objects.filter(reference=reference).first()
+        payment = Payment.objects.filter(reference=reference).first()
             
         if not payment:
             logger.warning(f"No payment found for reference: {reference}")
+            
+            verification = PaymentService.verify_payment(reference=reference)
+            
+            if verification:
+                logger.info("Recovered payment via provider")
+                
+            return HttpResponse(status=200)
+        
+        if payment.status == PaymentStatus.SUCCESS:
+            return HttpResponse(status=200)
             
         # store event
         PaymentEvent.objects.create(
@@ -55,11 +67,13 @@ class PayStackWebhookView(View):
         )
         
         # Handle successful payment
-        if event == "charge.success" and reference:
-            if payment:
-                PaymentService.verify_payment(reference)
-            else:
-                logger.warning(f"Skipping verification, payment not found: {reference}")
+        if event == "charge.success":
+            payment.status = PaymentStatus.SUCCESS
+            payment.save()
+            
+            order = payment.order
+            order.status = OrderStatus.PAID
+            order.save()
             
         return HttpResponse(status=200)
 
